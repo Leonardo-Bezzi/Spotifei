645e0efc78121255196afc528554dc42d83c0bc7
Leonardo Bezzi Elias <leonardobezzielias@gmail.com>
2025-05-20 17:03:52 -0300

    Agora com o backup do banco de dados no meu pc, e meu tecldo de casa extremmente falho, aperfeiccoei as funcoes de pesquisa e curtida, que esto funcionando perfeitmente

--- src/Model/DAOs/MusicaDAO.java
+++ src/Model/DAOs/MusicaDAO.java
@@ -12,18 +12,19 @@ import java.util.List;
  */
 public class MusicaDAO {
 
-    // Método genérico que busca por qualquer campo (nome, artista, gênero)
+    // Método que busca por qualquer campo (nome, artista, gênero)
     public List<Musica> buscarPorCampo(String campo, String termo, int idUsuario) {
         List<Musica> musicas = new ArrayList<>();
 
         if (!List.of("nome", "artista", "genero").contains(campo)) return musicas;
 
         String sql = """
-            SELECT m.*, 
-                   CASE WHEN c.id IS NOT NULL THEN true ELSE false END AS curtida
-            FROM musicas m
-            LEFT JOIN curtidas c ON m.id = c.id_musica AND c.id_usuario = ?
-            WHERE m.""" + campo + " ILIKE ?";
+        SELECT m.*, 
+               CASE WHEN c.id_musica IS NOT NULL THEN true ELSE false END AS curtida
+        FROM musicas m
+        LEFT JOIN curtidas c ON m.id = c.id_musica AND c.id_usuario = ?
+        WHERE m.""" + campo + " ILIKE ?";
+
 
         try (Connection conn = Conexao.getConnection();
              PreparedStatement pst = conn.prepareStatement(sql)) {
@@ -52,7 +53,7 @@ public class MusicaDAO {
     }
     
     public void adicionarCurtida(int idUsuario, int idMusica) {
-        String sql = "INSERT INTO curtidas (id_usuario, id_musica) VALUES (?, ?) ON CONFLICT DO NOTHING";
+        String sql = "INSERT INTO curtidas (id_usuario, id_musica, curtida) VALUES (?, ?, true) ON CONFLICT DO NOTHING";
 
         try (Connection conn = Conexao.getConnection();
              PreparedStatement pst = conn.prepareStatement(sql)) {
@@ -63,6 +64,7 @@ public class MusicaDAO {
             System.out.println("Erro ao adicionar curtida: " + e.getMessage());
         }
     }
+
 
     public void removerCurtida(int idUsuario, int idMusica) {
         String sql = "DELETE FROM curtidas WHERE id_usuario = ? AND id_musica = ?";
